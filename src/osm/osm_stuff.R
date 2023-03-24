c5997c9530598bdffe7618ef023cfa1573afbb02
Gopal Penny <gopalpenny@gmail.com>
2023-03-24 16:46:14 +0800

    minor changes

--- src/osm/osm_stuff.R
+++ src/osm/osm_stuff.R
@@ -6,28 +6,33 @@ library(osmdata)
 library(sf)
 library(ggplot2)
 
-q <- opq(bbox = "Battambang, Cambodia") %>%
-  add_osm_feature(key = c("waterway","natural"))
+# q <- 
 
-
-q <- 
-
-btm_water <- opq(bbox = "Battambang, Cambodia") %>%
+# btm_water <- opq(bbox = "Battambang, Cambodia") %>%
+btm_water <- opq(bbox = "Cambodia") %>%
   add_osm_feature(key = "natural", value = "water") %>%
-  osmdata_sf()
+  osmdata_sf() %>% magrittr::extract2("osm_polygons")
 
+# osm_lines(btm_water)
+
+# btm_waterways<- opq(bbox = "Battambang, Cambodia") %>%
 btm_waterways<- opq(bbox = "Battambang, Cambodia") %>%
-  add_osm_feature(key = "waterway") %>%
-  osmdata_sf()
+  add_osm_feature(key = "waterway", value = c("river","canal","stream","ditch","drain")) %>%
+  osmdata_sf() %>% magrittr::extract2("osm_lines")
 # head(q$available_features())
 
-ggp::obj_size(btm)
+# ggp::obj_size(btm_water)
+# unique(btm_waterways$waterway)
 
-names(btm$osm_lines)
+# btm_waterways$osm_lines %>% dplyr::filter(waterway == "flow_control")
+names(btm$btm_water)
 ggplot() + 
-  geom_sf(data = btm_waterways$osm_lines, aes(color = waterway)) +
-  geom_sf(data = btm_water$osm_polygons, aes(fill = water), color = NA)
+  # geom_sf(data = btm_waterways$osm_points, aes(color = waterway)) +
+  geom_sf(data = btm_waterways, aes(color = waterway)) +
+  geom_sf(data = btm_water, aes(fill = water), color = NA) +
+  coord_sf(xlim = c(102, 104.5), ylim = c(12, 14))
 
+btm_waterways %>% magrittr::extract2("osm_lines")
 
 ggplot() + 
   # geom_sf(data = btm$osm_lines, aes(color = waterway)) +
