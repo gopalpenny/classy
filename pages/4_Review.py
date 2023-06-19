6ab38d306dfa573d5bc09c768a7ef67e26aceca9
Gopal Penny <gopalpenny@gmail.com>
2023-06-19 12:39:21 -0400

    Subset option added to Review

--- pages/4_Review.py
+++ pages/4_Review.py
@@ -128,7 +128,14 @@ def prep_label_timeseries_all(class_df_long, generate_ts = False, timeseries_dir
     return ts_s1, ts_s2, ts_landsat
 
 # %%
+print('hello world')
 ts_s1, ts_s2, ts_landsat = prep_label_timeseries_all(class_df_long, generate_ts = False, timeseries_dir_path = timeseries_dir_path)
+
+# %%
+
+ts_s1_long = ts_s1 \
+    .assign(group_var = lambda x: x['loc_id'].astype(str) + '_' + x['Year'].astype(str)) \
+    .query('backscatter != Inf')
 
 # %%
 Subclass_options = pd.unique(ts_s1.Subclass)
@@ -137,59 +144,110 @@ s2_bands = ['B8','B4','B3','B2','NDVI']
 landsat_bands = ['swir2','swir1','nir','red','green','blue','NDVI']
 
 with st.sidebar:
-    st.multiselect('Subclasses', options = Subclass_options)
-    st.multiselect('S1 bands', options = s1_bands)
-    st.multiselect('S2 bands', options = s2_bands)
-    st.multiselect('Landsat bands', options = landsat_bands)
+    st.multiselect('Subclasses', options = Subclass_options, key = 'review_subclasses')
+    st.multiselect('S1 bands', options = s1_bands, key = 'review_s1_bands')
+    st.multiselect('S2 bands', options = s2_bands, key = 'review_s2_bands')
+    st.multiselect('Landsat bands', options = landsat_bands, key = 'review_landsat_bands')
+    review_subset_expander = st.expander('Subset data', expanded = False)
+
+Subclass_vals = st.session_state['review_subclasses']
+s1_vars = st.session_state['review_s1_bands']
+s2_vars = st.session_state['review_s2_bands']
+landsat_vars = st.session_state['review_landsat_bands']
+
+# %%
+with review_subset_expander:
+    st.text_input('S1 pandas query', key = 'subset_s1_query')
+    st.text_input('S2 pandas query', key = 'subset_s2_query')
+    st.text_input('Landsat pandas query', key = 'subset_landsat_query')
+
+s1_query = st.session_state['subset_s1_query'] if st.session_state['subset_s1_query'] != '' else 'backscatter != Inf'
+s2_query = st.session_state['subset_s2_query'] if st.session_state['subset_s2_query'] != '' else 'NDVI != Inf'
+landsat_query = st.session_state['subset_landsat_query'] if st.session_state['subset_landsat_query'] != '' else 'nir != Inf'
+
+if len(Subclass_vals) == 0:
+    st.markdown('### Please select subclasses in the sidebar')
+
+if all([len(s1_vars) == 0, len(s2_vars) == 0, len(landsat_vars) == 0]):
+    st.markdown('### Please select bands from at least one satellite mission')
+
+text_angle = 0
+text_hjust = 0.5
+
+# %%
+# Plot Sentinel 1 timeseries
+if len(Subclass_vals) > 0 and len(s1_vars) > 0:
+    try:
+        ts_s1_prep = ts_s1.query(s1_query)
+    except:
+        st.warning('Ignoring invalid S1 query -- must be valid pandas.DataFrame.query() string.')
+        ts_s1_prep = ts_s1
+
+    ts_s1_long = ts_s1_prep \
+        .query('Subclass in @Subclass_vals') \
+        .melt(id_vars = ['loc_id','Year','Subclass','date_yearless'], value_vars=s1_vars) \
+        .assign(group_var = lambda x: x['loc_id'].astype(str) + '_' + x['Year'].astype(str))
+    p_s1 = (p9.ggplot() + 
+    p9.geom_line(data = ts_s1_long, 
+                mapping = p9.aes(x = 'date_yearless', y = 'value', group = 'group_var'), alpha = 0.1) +
+    # p9.geom_smooth(data = ts_s1_long, mapping = p9.aes(x = 'date_yearless', y = 'value'), color = 'black', size = 3) +
+    p9.facet_grid('variable ~ Subclass', scales = 'free_y') +
+    p9.scale_x_datetime(date_breaks = '3 months', date_labels = '%b') +
+    p9.theme(axis_text_x = p9.element_text(angle = text_angle, hjust =text_hjust),
+                figure_size = (8,len(s1_vars)),
+                axis_title = p9.element_blank()))
+    
+    st.markdown('### S1 timeseries')
+    st.pyplot(p9.ggplot.draw(p_s1)) 
+
+# %%
+# Plot Sentinel 2 timeseries
+if len(Subclass_vals) > 0 and len(s2_vars) > 0:
+    try: 
+        ts_s2_prep = ts_s2.query(s2_query)
+    except:
+        st.warning('Ignoring invalid S2 query -- must be valid pandas.DataFrame.query() string')
+        ts_s2_prep = ts_s2
+
+    ts_s2_long = ts_s2_prep \
+        .query('Subclass in @Subclass_vals') \
+        .melt(id_vars = ['loc_id','Year','Subclass','date_yearless'], value_vars=s2_vars) \
+        .assign(group_var = lambda x: x['loc_id'].astype(str) + '_' + x['Year'].astype(str))
+    p_s2 = (p9.ggplot() + 
+    p9.geom_line(data = ts_s2_long, 
+                mapping = p9.aes(x = 'date_yearless', y = 'value', group = 'group_var'), alpha = 0.1) +
+    # p9.geom_smooth(data = ts_s2_long, mapping = p9.aes(x = 'date_yearless', y = 'value'), color = 'black', size = 3) +
+                p9.facet_grid('variable ~ Subclass', scales = 'free_y') +
+                p9.scale_x_datetime(date_breaks = '3 months', date_labels = '%b') +
+                p9.theme(axis_text_x = p9.element_text(angle = text_angle, hjust =text_hjust),
+                            figure_size = (8,len(s2_vars)),
+                            axis_title = p9.element_blank()))
+    st.markdown('### S2 timeseries')
+    st.pyplot(p9.ggplot.draw(p_s2))
 
 
 # %%
-Subclass_vals = ['Single crop','Double crop','Plantation']
-s1_vars = ['backscatter']
-ts_s1_long = ts_s1 \
-    .query('Subclass in @Subclass_vals') \
-    .melt(id_vars = ['loc_id','Year','Subclass','date_yearless'], value_vars=s1_vars) 
+# Plot Landsat timeseries
+if len(Subclass_vals) > 0 and len(landsat_vars) > 0:
+    try:
+        ts_landsat_prep = ts_landsat.query(landsat_query)
+    except:
+        st.warning('Ignoring invalid Landsat query -- must by valid pandas.DataFrame.query() string')
+        ts_landsat_prep = ts_landsat
 
+    ts_landsat_long = ts_landsat_prep \
+        .query('Subclass in @Subclass_vals') \
+        .melt(id_vars = ['loc_id','Year','Subclass','date_yearless'], value_vars=landsat_vars) \
+        .assign(group_var = lambda x: x['loc_id'].astype(str) + '_' + x['Year'].astype(str))
+
+    p_landsat = (p9.ggplot() + 
+    p9.geom_line(data = ts_landsat_long, 
+                mapping = p9.aes(x = 'date_yearless', y = 'value', group = 'group_var'), alpha = 0.1) +
+                p9.facet_grid('variable ~ Subclass', scales = 'free_y') +
+                p9.scale_x_datetime(date_breaks = '3 months', date_labels = '%b') +
+                p9.theme(axis_text_x = p9.element_text(angle = text_angle, hjust =text_hjust),
+                            figure_size = (8,len(landsat_vars)),
+                            axis_title = p9.element_blank()))
+    st.markdown('### Landsat timeseries')
+    st.pyplot(p9.ggplot.draw(p_landsat))
 # %%
-p_s1 = (p9.ggplot() + 
- p9.geom_line(data = ts_s1_long, 
-               mapping = p9.aes(x = 'date_yearless', y = 'value', group = 'loc_id'), alpha = 0.1) +
-               p9.facet_grid('variable ~ Subclass', scales = 'free_y') +
-               p9.scale_x_datetime(date_breaks = '3 months', date_labels = '%b') +
-               p9.theme(axis_text_x = p9.element_text(angle = 45, hjust =1),
-                        figure_size = (8,3),
-                        axis_title_x = p9.element_blank()))
-# %%
-s2_vars = ['NDVI']
-ts_s2_long = ts_s2 \
-    .query('Subclass in @Subclass_vals') \
-    .melt(id_vars = ['loc_id','Year','Subclass','date_yearless'], value_vars=s2_vars) 
-p_s2 = (p9.ggplot() + 
- p9.geom_line(data = ts_s2_long, 
-               mapping = p9.aes(x = 'date_yearless', y = 'value', group = 'loc_id'), alpha = 0.1) +
-               p9.facet_grid('variable ~ Subclass', scales = 'free_y') +
-               p9.scale_x_datetime(date_breaks = '3 months', date_labels = '%b') +
-               p9.theme(axis_text_x = p9.element_text(angle = 45, hjust =1),
-                        figure_size = (8,3),
-                        axis_title_x = p9.element_blank()))
-# %%
-landsat_vars = ['NDVI','swir1']
-ts_landsat_long = ts_landsat \
-    .query('Subclass in @Subclass_vals') \
-    .melt(id_vars = ['loc_id','Year','Subclass','date_yearless'], value_vars=landsat_vars)
-
-p_landsat = (p9.ggplot() + 
- p9.geom_line(data = ts_landsat_long, 
-               mapping = p9.aes(x = 'date_yearless', y = 'value', group = 'loc_id'), alpha = 0.1) +
-               p9.facet_grid('variable ~ Subclass', scales = 'free_y') +
-               p9.scale_x_datetime(date_breaks = '3 months', date_labels = '%b') +
-               p9.theme(axis_text_x = p9.element_text(angle = 45, hjust =1),
-                        figure_size = (8,3),
-                        axis_title_x = p9.element_blank()))
-# %%
-
-st.pyplot(p9.ggplot.draw(p_s1)) 
-
-st.pyplot(p9.ggplot.draw(p_s2))
-
-st.pyplot(p9.ggplot.draw(p_landsat))
